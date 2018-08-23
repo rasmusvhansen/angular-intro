951f402acd3f3feb4aa8dd0b8222a1d2504e9e5e
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2018-08-23 14:22:10 +0200

    Removed service

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy } from '@angular/core';
-import { YoutubeService } from '../youtube.service';
 import { Video } from '../types';
 
 @Component({
@@ -10,8 +9,23 @@ import { Video } from '../types';
 export class VideoListComponent implements OnDestroy {
   public videos: Video[];
 
-  constructor(private youtubeService: YoutubeService) {
-    this.videos = this.youtubeService.search('not used');
+  constructor() {
+    this.videos = [
+      {
+        title: 'Angular in 60 Minutes',
+        description: 'Learn angular in 60 minutes',
+        thumb: 'https://i.ytimg.com/vi/KhzGSHNhnbI/mqdefault.jpg',
+        link: 'https://www.youtube.com/watch?v=KhzGSHNhnbI'
+      },
+      {
+        title: 'Angular vs React.js vs Vue.js - My Thoughts!',
+        description:
+          'Angular, React.js or Vue.js? I get this question a lot! Let me share some thoughts on it with you! ' +
+          'Early bird offer - Join my course on this topic for only $10: ',
+        thumb: 'https://i.ytimg.com/vi/KMX1mFEmM3E/mqdefault.jpg',
+        link: 'https://www.youtube.com/watch?v=KMX1mFEmM3E'
+      }
+    ];
   }
 
   ngOnDestroy() {}
---
+++ src/youtube links.md
@@ -0,0 +1,2 @@
+const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=';
+const apiKey = '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8';
