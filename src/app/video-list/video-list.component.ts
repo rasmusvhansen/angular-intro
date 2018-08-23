69408f0162eb0c89781cc2a5101859df8a0bc13a
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2018-08-23 09:47:11 +0200

    Misc Change route on search Added hover animation Tweaked styling

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { YoutubeService, Video } from '../youtube.service';
-import { Observable, merge } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap, map, debounceTime, filter } from 'rxjs/operators';
+import { Observable, merge, Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap, map, debounceTime, filter, tap } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -10,18 +10,23 @@ import { FormControl } from '@angular/forms';
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
+export class VideoListComponent implements OnInit, OnDestroy {
+  private userSearchSubscription: Subscription;
   public videos$: Observable<Video[]>;
   public query = new FormControl('');
 
-  constructor(private youtubeService: YoutubeService, private route: ActivatedRoute) {
-    const userSearch$ = this.query.valueChanges;
+  constructor(private youtubeService: YoutubeService, private route: ActivatedRoute, private router: Router) {
+    this.userSearchSubscription = this.query.valueChanges.subscribe(q => this.router.navigate(['videos', q]));
     const routeSearch$ = this.route.paramMap.pipe(map(param => param.get('query')));
-    this.videos$ = merge(routeSearch$, userSearch$).pipe(
+    this.videos$ = routeSearch$.pipe(
       filter(q => q.length > 1),
       debounceTime(500),
       switchMap(q => this.youtubeService.search(q))
     );
+  }
+
+  ngOnDestroy() {
+    this.userSearchSubscription.unsubscribe();
   }
 
   ngOnInit() {}
