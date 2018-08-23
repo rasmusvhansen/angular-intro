abc7faa12f7e8208a7ac0d61b7472a28ba55c1c3
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2018-08-23 14:37:25 +0200

    Added type to video array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Video } from './types';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Intro';
 
-  videos = [
+  videos: Video[] = [
     {
       title: 'Angular in 60 Minutes',
       description: 'Learn angular in 60 minutes',
