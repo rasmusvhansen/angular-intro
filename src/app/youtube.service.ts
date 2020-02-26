9fa8ffee7a30e56525c4e13ca0649ba7085d9ba3
Rasmus Vestergaard Hansen <rasv@danskebank.dk>
2020-02-26 10:33:52 +0100

    fix: removed api key

--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs';
 import { YoutubeResult, Video } from './types';
 import { SearchParams } from './video-list/video-list.component';
 
-const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&type=video&q=';
-const apiKey = '&key=AIzaSyAKyu021t7N6TSwa1YWJjK8zsaK3aTT92w';
+const API_KEY = 'paste your api key here, get a key at https://console.developers.google.com/apis/api/youtube.googleapis.com';
+const baseUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${API_KEY}&q=`;
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,7 @@ export class YoutubeService {
   constructor(private httpClient: HttpClient) {}
 
   search(params: SearchParams): Observable<Array<Video>> {
-    const url =
-      baseUrl + params.query + `&publishedAfter=${new Date(Date.now() - params.age * 24 * 60 * 60 * 1000).toISOString()}` + apiKey;
+    const url = baseUrl + params.query + `&publishedAfter=${new Date(Date.now() - params.age * 24 * 60 * 60 * 1000).toISOString()}`;
     console.log(`Searching for ${params.query} with age: ${params.age}`);
 
     // Calls the YouTube API and transforms the result. Note that nothing happens until the Observable is subscribed to
